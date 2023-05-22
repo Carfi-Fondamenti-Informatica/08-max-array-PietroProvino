aee5ab84e43c7db416a4e5c086672ed9d81d266e
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-22 12:53:33 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+float Max(int n, float a[]){
+    float b;
+    b=a[0];
+    for(int i=0;i<n;i++){
+        for(int j=0; j<i;j++){
+            if(a[i]<a[j]){
+                b=a[i];
+                a[i]=a[j];
+                a[j]=b;
+            }
+        }
+    }
+    return a[n-1];
+}
