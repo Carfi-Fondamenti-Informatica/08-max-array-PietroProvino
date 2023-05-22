96d9d275ddd8f38ca883dc33715b9fdc294d308a
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-22 12:54:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n;
+    do{
+        cin >> n;
+    }while (n<=1);
+    float a[n], b ;
+    for (int i=0; i<n; i++){
+        cin >> b;
+        a[i]=b;
+    }
+    cout << Max(n , a);
+    return 0;
 }
